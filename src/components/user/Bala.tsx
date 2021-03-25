bbcb1366e771cded4fc136f712c4aff9d8ea145c
balaji kesavan <contactbalajik7@icloud.com>
2021-03-25 06:20:53 +0530

    Updated with an upload image file func and google fonts

--- src/components/user/Bala.tsx
+++ src/components/user/Bala.tsx
@@ -2,9 +2,20 @@ import React from 'react';
 import './Bala.scss';
 
 function Bala(){
+
     return(
         <div className="Bala">
-            <p> section banner </p>
+            <p> Section banner </p>
+            <div className = "gftext">
+                <label>Select a Image file : </label>
+                <br />
+                <input type="file" multiple />
+                <br />
+                <button className = "button" >
+                    Upload
+                </button>
+            </div>
+
         </div>
     );
 }
